912359ffb27187d6f8bc04eb9b19de816b11bdf2
jiajing0521 <757959688@qq.com>
2016-11-25 23:42:16 +0800

    home

--- NodeJs/99.DEMO/newsWeb/control.js
+++ NodeJs/99.DEMO/newsWeb/control.js
@@ -10,14 +10,50 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+//连接数据库
+var connect = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "",
+    database: "0811"
+});
+connect.connect();
+
 //设置模板引擎
 app.set("view engine","ejs");
 // 设置模板路径
 app.set("views","./views");
 app.use(express.static("static"));
 
+//home
 app.get("/",(req,res)=>{
-    res.render(path.join(__dirname,"views/home.ejs"),{});
+	connect.query("SELECT * FROM news ORDER BY time LIMIT 5",(err,rows,field)=>{
+		if(err){
+			throw err;
+		}
+		res.render(path.join(__dirname,"views/home.ejs"),{
+			news:rows
+		});
+	})
 });
+
+//detail
+app.get("/detail/:id",(req,res)=>{
+	connect.query(`SELECT * FROM news WHERE id = ${res.params.id}`,(err,rows,fiels)=>{
+		if(err){
+			throw err;
+		}
+		res.render(path.join(__dirname,"views/detail.ejs"),{
+			new: row[0]
+		})
+	})
+});
+
+///upload
+app.post("/upload",(req,res)=>{
+	res.render(path.join(__dirname,"views/upload.ejs"),{
+			
+		})
+})
 
 app.listen(8888);
